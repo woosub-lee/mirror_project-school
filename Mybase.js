e926ab07c101d1f87186cb57e0c0c71f350752c6
woosub-lee <woosub.project@gmail.com>
2020-12-22 20:32:40 +0900

    fix firebase

--- Mybase.js
+++ Mybase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore"
 
 var firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_ID,
-    appId: process.env.APP_ID
+    apiKey: process.env.FIRE_BASE_API_KEY,
+    authDomain: process.env.FIRE_BASE_AUTH_DOMAIN,
+    projectId: process.env.FIRE_BASE_PROJECT_ID,
+    storageBucket: process.env.FIRE_BASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRE_BASE_MESSAGING_ID,
+    appId: process.env.FIRE_BASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
